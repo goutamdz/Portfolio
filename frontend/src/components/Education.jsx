bbb376042ac10a8bb7a0f9a32c317d3eb760c264
goutamdz <lemmesingonce@gmail.com>
2025-04-21 18:37:28 +0530

    fixed: typo in education section

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -12,7 +12,7 @@ const Education = () => {
       <div className="max-w-5xl mx-auto">
         <h2 className="text-4xl font-bold text-center mb-10">Education</h2>
 
-        <ExperienceCard logo={university} name={"Lovely Professional University, Punjab"} grade={"B.Tech - 8.11 CGPA"} address={"Jalandhar, Punjab-144411"} startDate={"Aug 2022"} endDate="present"/>
+        <ExperienceCard logo={university} name={"Lovely Professional University, Punjab"} grade={"B.Tech - CSE - 8.11 CGPA"} address={"Jalandhar, Punjab-144411"} startDate={"Aug 2022"} endDate="present"/>
         
         <ExperienceCard logo={Jharkhand} name={"Plus Two High School Atka"} grade={"XII - PCM - 93.2%"} address={"Atka, Giridih, Jharkhand-825322"} startDate={"Aug 2020"} endDate="July 2022"/>
 
